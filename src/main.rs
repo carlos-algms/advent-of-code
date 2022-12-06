c67e422898a91b5a4bd2543792ebebfb70810fb5
Carlos Gomes <carlos.algms@gmail.com>
2022-12-06 20:28:35 +0100

    Restructuring the Repo to support Rust file structure

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+#![allow(unused_imports)]
+mod year_2022;
+
+use year_2022::day_01::run_day_01;
+
+fn main() {
+    run_day_01();
+}
---
+++ src/year_2022/day_01/mod.rs
@@ -0,0 +1,8 @@
+pub fn run_day_01() {
+    println!("running day 01");
+}
+
+#[allow(dead_code)]
+fn main() {
+    run_day_01();
+}
