53bb138161e959fedc8ae5b7844357af704aaf0f
Carlos Gomes <carlos.algms@gmail.com>
2023-12-02 19:16:07 +0100

    Day 02 Part 1 working solution

--- years/year_2023/day_02/day_02_part1.test.ts
+++ years/year_2023/day_02/day_02_part1.test.ts
@@ -2,15 +2,37 @@ import assert from 'assert';
 
 import { fileReader } from '../shared/fileReader';
 
-import { day01Part1 } from './day_02_part1';
+import { BagContent, day02Part1 } from './day_02_part1';
+
+const load: BagContent = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
 
 async function part1TestInput() {
-  const input = await fileReader(__dirname + '/day_01_input_1_test.txt');
+  const input = await fileReader(__dirname + '/day_02_input_1_test.txt');
+
+  const { sum, ids } = day02Part1(input, load);
+
+  const expectedSum = 8;
+  const expectedIds = ['1', '2', '5'];
+
+  assert.strictEqual(sum, expectedSum);
+  assert.deepStrictEqual(ids, expectedIds);
+
   console.log('✅ Test input passed!');
 }
 
 async function part1Input() {
-  const input = await fileReader(__dirname + '/day_01_input.txt');
+  const input = await fileReader(__dirname + '/day_02_input.txt');
+
+  const { sum } = day02Part1(input, load);
+
+  const expectedSum = 8;
+
+  assert.equal(sum, expectedSum);
+
   console.log('✅ Part 1 input passed!');
 }
 
--- years/year_2023/day_02/day_02_part1.ts
+++ years/year_2023/day_02/day_02_part1.ts
@@ -1,3 +1,41 @@
-export function day01Part1(input: string[]) {
-  return true;
+export type Day2Part1ReturnValue = {
+  sum: number;
+  ids: string[];
+};
+
+export type BagContent = {
+  red: number;
+  green: number;
+  blue: number;
+};
+
+export function day02Part1(input: string[], bagLoad: BagContent): Day2Part1ReturnValue {
+  const result: Day2Part1ReturnValue = {
+    sum: 0,
+    ids: [],
+  };
+
+  for (const line of input) {
+    const [game, roundsData] = line.split(': ');
+
+    const rounds = roundsData.split('; ');
+    const isPossible = rounds.every((round) => {
+      const colors = round.split(', ');
+
+      return colors.every((color) => {
+        const [colorCount, colorName] = color.split(' ');
+        const count = parseInt(colorCount);
+        const loadedCount = bagLoad[colorName as keyof BagContent];
+        return loadedCount && loadedCount >= count;
+      });
+    });
+
+    if (isPossible) {
+      const [_, id] = game.split(' ');
+      result.ids.push(id);
+      result.sum += parseInt(id);
+    }
+  }
+
+  return result;
 }
