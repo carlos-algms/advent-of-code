20a118dd4cea0fc62835ebef49e9f1fc379ad677
Carlos Gomes <carlos.algms@gmail.com>
2023-12-13 12:50:05 +0100

    Day 12 - part 1 - frustrated implementation

--- years/year_2023/day_12/day_12_part1.run.ts
+++ years/year_2023/day_12/day_12_part1.run.ts
@@ -2,9 +2,45 @@ import assert from 'assert';
 
 import { fileReader } from '../shared/fileReader';
 
-import solution from './day_12_part1';
+import solution, { countArrangements, removeExtraDots } from './day_12_part1';
 
 const day = '12';
+
+function testSpecificArrangement() {
+  const arrangement = removeExtraDots('.##..#....###.');
+  const sequence = [1, 1, 3];
+  const memo = new Map<string, number>();
+  const result = countArrangements(arrangement.split(''), sequence, memo);
+  assert.equal(result, 1, 'the sum');
+  console.log(`✅ Day ${day} - part 1 - specific 1 passed!`);
+}
+
+function testWith1Arrangement() {
+  const input = ['???.### 1,1,3'];
+  const response = solution(input);
+
+  assert.equal(response, 1, 'the sum');
+
+  console.log(`✅ Day ${day} - part 1 - test with 1 passed!`);
+}
+
+function testWith4Arrangements() {
+  const input = ['.??..??...?##. 1,1,3'];
+  const response = solution(input);
+
+  assert.equal(response, 4, 'the sum');
+
+  console.log(`✅ Day ${day} - part 1 - test with 4 passed!`);
+}
+
+function testWith10Arrangements() {
+  const input = ['?###???????? 3,2,1'];
+  const response = solution(input);
+
+  assert.equal(response, 10, 'the sum');
+
+  console.log(`✅ Day ${day} - part 1 - test with 10 passed!`);
+}
 
 async function test1Input() {
   const input: string[] = `???.### 1,1,3
@@ -32,6 +68,10 @@ async function realInput() {
 }
 
 (async () => {
-  await test1Input();
+  // testSpecificArrangement();
+  // testWith1Arrangement();
+  // testWith4Arrangements();
+  testWith10Arrangements();
+  // await test1Input();
   // await realInput();
 })();
--- years/year_2023/day_12/day_12_part1.ts
+++ years/year_2023/day_12/day_12_part1.ts
@@ -1,3 +1,108 @@
-export default function day11Part1(lines: readonly string[]) {
-  return 0;
+/**
+ *
+    ???.### 1,1,3             - 1 arrangement
+    .??..??...?##. 1,1,3      - 4 arrangements
+    ?#?#?#?#?#?#?#? 1,3,1,6   - 1 arrangement
+    ????.#...#... 4,1,1       - 1 arrangement
+    ????.######..#####. 1,6,5 - 4 arrangements
+    ?###???????? 3,2,1        - 10 arrangements
+
+ */
+export default function day12Part1(lines: readonly string[]): number {
+  const result = lines.reduce((sum, lineContent, i) => {
+    const [line, sequence] = lineContent.split(' ');
+    const uniqueLinesSet = new Set<string>();
+    buildUniqueLines(line.split(''), uniqueLinesSet);
+
+    let lineSum = 0;
+    const lineMemo = new Map<string, number>();
+
+    uniqueLinesSet.forEach((uniqueLine) => {
+      const cleanLine = removeExtraDots(uniqueLine);
+      const uniqueSum = countArrangements(
+        cleanLine.split(''),
+        sequence.split(',').map(Number),
+        lineMemo,
+      );
+      lineSum += uniqueSum;
+    });
+
+    return sum + lineSum;
+  }, 0);
+
+  return result;
+}
+
+export function countArrangements(
+  line: string[],
+  sameInSequence: number[],
+  memo: Map<string, number>,
+) {
+  const sequenceLength = sameInSequence.length;
+  const maxPositionToMatch = line.length - sequenceLength;
+  let sum = 0;
+
+  for (let x1 = 0; x1 <= maxPositionToMatch; x1++) {
+    let s = 0;
+    let wantedChar = '#';
+    let sequence = '';
+    let fullSequence = '';
+
+    for (let x2 = x1; x2 < line.length; x2++) {
+      const char = line[x2];
+
+      if (char !== wantedChar) {
+        break;
+      }
+
+      fullSequence += char;
+
+      if (char === '.') {
+        wantedChar = '#';
+        continue;
+      }
+
+      sequence += char;
+
+      if (sequence.length === sameInSequence[s]) {
+        s++;
+        wantedChar = '.';
+        sequence = '';
+      }
+
+      if (s === sequenceLength) {
+        sum++;
+        break;
+      }
+    }
+
+    fullSequence;
+  }
+
+  return sum;
+}
+
+export function removeExtraDots(line: string) {
+  return line.replace(/^\.+|\.+$/g, '').replace(/\.{2,}/g, '.');
+}
+
+const Joker = '?';
+const PossibleChars = ['#', '.'];
+
+function buildUniqueLines(line: string[], uniques: Set<string>) {
+  for (let c = 0; c < line.length; c++) {
+    const char = line[c];
+
+    if (char === Joker) {
+      for (const possibleChar of PossibleChars) {
+        const newLine = [...line];
+        newLine[c] = possibleChar;
+        if (!newLine.includes(Joker)) {
+          uniques.add(newLine.join(''));
+        } else {
+          buildUniqueLines(newLine, uniques);
+        }
+      }
+    }
+  }
 }
