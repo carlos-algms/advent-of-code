e22e6f751a59f6ce9c22bea54e1d60fe064b2701
Carlos Gomes <carlos.algms@gmail.com>
2023-12-15 13:19:53 +0100

    Day 14 - part 2 - solution

--- years/year_2023/day_14/day_14_part2.run.ts
+++ years/year_2023/day_14/day_14_part2.run.ts
@@ -29,7 +29,7 @@ async function realInput() {
   const input = await fs.readFile(__dirname + `/day_${day}_input.txt`, 'utf8');
 
   const response = solution(input);
-  assert.equal(response, 0, 'the sum');
+  assert.equal(response, 118747, 'the sum');
 
   console.log(`✅ Day ${day} - part ${part} - real input passed!`);
 }
