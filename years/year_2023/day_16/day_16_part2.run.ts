71f312cdf61a676d23fc001dacfc3640f4c78276
Carlos Gomes <carlos.algms@gmail.com>
2023-12-16 19:17:01 +0100

    Day 16 - part 2 - setup

--- years/year_2023/day_16/day_16_part2.run.ts
+++ years/year_2023/day_16/day_16_part2.run.ts
@@ -1,19 +1,19 @@
 import assert from 'node:assert';
 import fs from 'node:fs/promises';
 
-import solution from './day_16_part1';
+import solution from './day_16_part2';
 
 const day = '16';
-const part = '1';
+const part = '2';
 
 async function test1Input() {
   const input = await fs.readFile(__dirname + `/day_${day}_input_test.txt`, 'utf8');
 
-  console.time('test 1');
+  console.time('test 2');
   const response = solution(input);
-  console.timeEnd('test 1');
+  console.timeEnd('test 2');
 
-  assert.equal(response, 46, 'the sum');
+  assert.equal(response, 51, 'the sum');
 
   console.log(`✅ Day ${day} - part ${part} - test 1 passed!`);
 }
@@ -25,12 +25,12 @@ async function realInput() {
   const response = solution(input);
   console.timeEnd('real input');
 
-  assert.equal(response, 8389, 'the sum');
+  assert.equal(response, -Infinity, 'the sum');
 
   console.log(`✅ Day ${day} - part ${part} - real input passed!`);
 }
 
 (async () => {
   await test1Input();
-  await realInput();
+  // await realInput();
 })();
---
+++ years/year_2023/day_16/day_16_part2.ts
@@ -0,0 +1,97 @@
+const enum Direction {
+  Up = 'U',
+  Down = 'D',
+  Left = 'L',
+  Right = 'R',
+}
+
+type Grid = string[][];
+
+/**
+ *
+ */
+export default function day16Part2(input: string): number {
+  /** Holds the coord line,row of the lines with beans passing */
+  const energized = new Set<string>();
+  const directionCache = new Set<string>();
+
+  const grid = input.split('\n').map((line) => line.split(''));
+  move(0, 0, Direction.Right, grid, energized, directionCache);
+
+  return energized.size;
+}
+
+// TODO make it async and each direction can flow in parallel
+function move(
+  line: number,
+  column: number,
+  direction: Direction,
+  grid: Grid,
+  coordCounter: Set<string>,
+  directionCache: Set<string>,
+) {
+  const current = grid[line]?.[column];
+  if (!current) {
+    return;
+  }
+
+  const key = `${line},${column},${direction}`;
+
+  if (directionCache.has(key)) {
+    return;
+  }
+
+  directionCache.add(key);
+  coordCounter.add(`${line},${column}`);
+
+  switch (direction) {
+    case Direction.Up:
+      if ('.|'.includes(current)) {
+        move(line - 1, column, Direction.Up, grid, coordCounter, directionCache);
+      } else if ('/' === current) {
+        move(line, column + 1, Direction.Right, grid, coordCounter, directionCache);
+      } else if ('\\' === current) {
+        move(line, column - 1, Direction.Left, grid, coordCounter, directionCache);
+      } else if ('-' === current) {
+        move(line, column - 1, Direction.Left, grid, coordCounter, directionCache);
+        move(line, column + 1, Direction.Right, grid, coordCounter, directionCache);
+      }
+      break;
+    case Direction.Down:
+      if ('.|'.includes(current)) {
+        move(line + 1, column, Direction.Down, grid, coordCounter, directionCache);
+      } else if ('/' === current) {
+        move(line, column - 1, Direction.Left, grid, coordCounter, directionCache);
+      } else if ('\\' === current) {
+        move(line, column + 1, Direction.Right, grid, coordCounter, directionCache);
+      } else if ('-' === current) {
+        move(line, column - 1, Direction.Left, grid, coordCounter, directionCache);
+        move(line, column + 1, Direction.Right, grid, coordCounter, directionCache);
+      }
+      break;
+    case Direction.Left:
+      if ('.-'.includes(current)) {
+        move(line, column - 1, Direction.Left, grid, coordCounter, directionCache);
+      } else if ('/' === current) {
+        move(line + 1, column, Direction.Down, grid, coordCounter, directionCache);
+      } else if ('\\' === current) {
+        move(line - 1, column, Direction.Up, grid, coordCounter, directionCache);
+      } else if ('|' === current) {
+        move(line - 1, column, Direction.Up, grid, coordCounter, directionCache);
+        move(line + 1, column, Direction.Down, grid, coordCounter, directionCache);
+      }
+      break;
+    case Direction.Right:
+      if ('.-'.includes(current)) {
+        move(line, column + 1, Direction.Right, grid, coordCounter, directionCache);
+      } else if ('/' === current) {
+        move(line - 1, column, Direction.Up, grid, coordCounter, directionCache);
+      } else if ('\\' === current) {
+        move(line + 1, column, Direction.Down, grid, coordCounter, directionCache);
+      } else if ('|' === current) {
+        move(line - 1, column, Direction.Up, grid, coordCounter, directionCache);
+        move(line + 1, column, Direction.Down, grid, coordCounter, directionCache);
+      }
+      break;
+  }
+}
