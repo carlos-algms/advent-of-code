55dfde81ca35f98135bf2f81fda91916f8bca751
Carlos Gomes <carlos.algms@gmail.com>
2023-12-18 20:24:40 +0100

    Day 18 - part 2 - setup

---
+++ years/year_2023/day_18/day_18_part2.run.ts
@@ -0,0 +1,46 @@
+import assert from 'node:assert';
+import fs from 'node:fs/promises';
+
+import solution from './day_18_part2';
+
+const day = '18';
+const part = '2';
+
+async function test1Input() {
+  const input: string = [
+    'R 6 (#70c710)',
+    'D 5 (#0dc571)',
+    'L 2 (#5713f0)',
+    'D 2 (#d2c081)',
+    'R 2 (#59c680)',
+    'D 2 (#411b91)',
+    'L 5 (#8ceee2)',
+    'U 2 (#caa173)',
+    'L 1 (#1b58a2)',
+    'U 2 (#caa171)',
+    'R 2 (#7807d2)',
+    'U 3 (#a77fa3)',
+    'L 2 (#015232)',
+    'U 2 (#7a21e3)',
+  ].join('\n');
+
+  const response = solution(input);
+
+  assert.equal(response, 952408144115, 'the sum');
+
+  console.log(`✅ Day ${day} - part ${part} - test 1 passed!`);
+}
+
+async function realInput() {
+  const input = await fs.readFile(__dirname + `/day_${day}_input.txt`, 'utf8');
+
+  const response = solution(input);
+  assert.equal(response, 0, 'the sum');
+
+  console.log(`✅ Day ${day} - part ${part} - real input passed!`);
+}
+
+(async () => {
+  await test1Input();
+  // await realInput();
+})();
