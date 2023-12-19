d37b8c9641130f0ebf3cd3342f19b4530b18bd14
Carlos Gomes <carlos.algms@gmail.com>
2023-12-19 23:03:57 +0100

    Day 19 - part 1 - setup

---
+++ years/year_2023/day_19/day_19_part1.run.ts
@@ -0,0 +1,47 @@
+import assert from 'node:assert';
+import fs from 'node:fs/promises';
+
+import solution from './day_19_part1';
+
+const day = '19';
+const part = '1';
+
+async function test1Input() {
+  const input: string = `px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}`;
+
+  const response = solution(input);
+
+  assert.equal(response, 19114, 'the sum');
+
+  console.log(`✅ Day ${day} - part ${part} - test 1 passed!`);
+}
+
+async function realInput() {
+  const input = await fs.readFile(__dirname + `/day_${day}_input.txt`, 'utf8');
+
+  const response = solution(input);
+  assert.equal(response, 0, 'the sum');
+
+  console.log(`✅ Day ${day} - part ${part} - real input passed!`);
+}
+
+(async () => {
+  await test1Input();
+  await realInput();
+})();
---
+++ years/year_2023/day_19/day_19_part1.ts
@@ -0,0 +1,6 @@
+/**
+ *
+ */
+export default function day19Part1(input: string): number {
+  return -Infinity;
+}
